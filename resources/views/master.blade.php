2df4828e6deacef79df38e821f7545f135e8ee53
zarulizham <zarul.izham@gmail.com>
2019-04-18 10:09:50 +0800

    Updating contents

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -7,43 +7,35 @@
     <meta name="description" content="Never missed any updates of your item by receiving through email! Currently only Poslaju and Skynet are supported.">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 
-    <!-- CSS Files -->
     <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" media="screen">
     <link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet">
     <link href="{{ asset('fonts/icon-7-stroke/css/pe-icon-7-stroke.css') }}" rel="stylesheet">
     <link href="{{ asset('css/animate.css') }}" rel="stylesheet" media="screen">
     <link href="{{ asset('css/owl.theme.css') }}" rel="stylesheet">
     <link href="{{ asset('css/owl.carousel.css') }}" rel="stylesheet">
-
     <link href="{{ asset('css/styles.css') }}" rel="stylesheet" media="screen">
 
-    <!-- Google Fonts -->
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Alegreya+Sans:100,300,400,700' rel='stylesheet' type='text/css'>
 </head>
 
 <body data-spy="scroll" data-target="#navbar-scroll">
-
     <div id="top"></div>
-
     <div class="fullscreen landing parallax" style="background-image:url('{{ asset("images/bg.jpg") }}');" data-img-width="2000" data-img-height="1325" data-diff="100">
 
         <div class="overlay">
             <div class="container">
                 <div class="row">
                     <div class="col-md-8 col-md-offset-2 text-center">
-
                         <h1 class="wow fadeInLeft">
-                            Business Collaboration
+                            Courier Tracking Notification
                         </h1>
-
                         <div class="landing-text wow fadeInUp">
-                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                            <p>Never missed any updates of your item by receiving through email! Currently only Poslaju and Skynet are supported.</p>
                         </div>
-
                         <div class="head-btn wow fadeInLeft">
-                            <a href="#" class="btn-primary">Products</a>
-                            <a href="#" class="btn-default">Services</a>
+                            <a href="#" class="btn-primary">How It Works</a>
+                            <a href="#" class="btn-default">Track Now</a>
                         </div>
                         <br />
                         <br />
@@ -52,8 +44,6 @@
             </div>
         </div>
     </div>
-
-    <!-- NAVIGATION -->
     <div id="menu">
         <nav class="navbar-wrapper navbar-default" role="navigation">
             <div class="container">
@@ -66,16 +56,11 @@
                     </button>
                     <a class="navbar-brand site-name" href="{{ route('home') }}"><img src="{{ asset('images/logo.png') }}" alt="logo"></a>
                 </div>
-
                 <div id="navbar-scroll" class="collapse navbar-collapse navbar-themers navbar-right">
                     <ul class="nav navbar-nav">
                         <li><a href="{{ route('home') }}">Home</a></li>
                         <li><a href="#services">Services</a></li>
-                        <li><a href="#about-us">About us</a></li>
-                        <li><a href="#portfolios">Portfolio</a></li>
-                        <li><a href="#clients">Clients</a></li>
-                        <li><a href="#pricing">Pricing</a></li>
-                        <li><a href="#contact">Contact</a></li>
+                        <li><a href="#tracking">Tracking</a></li>
                     </ul>
                 </div>
             </div>
@@ -87,7 +72,6 @@
     <footer id="footer">
         <div class="container">
             <div class="col-sm-4 col-sm-offset-4">
-
                 <div class="social text-center">
                     <ul>
                         <li><a class="wow fadeInUp" href="https://twitter.com/"><i class="fa fa-twitter"></i></a></li>
@@ -101,7 +85,6 @@
         </div>
     </footer>
 
-    <!-- javascript files -->
     <script src="{{ asset('js/jquery.js') }}"></script>
     <script src="{{ asset('js/bootstrap.min.js') }}"></script>
     <script src="{{ asset('js/custom.js') }}"></script>
