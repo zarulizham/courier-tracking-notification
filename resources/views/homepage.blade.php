05f07074aff6365f27ead98511135c900c2635af
zarulizham <zarul.izham@gmail.com>
2019-06-21 11:28:51 +0800

    Adding privacy policy

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -73,4 +73,38 @@
         </div>
     </div>
 </div>
+<div id="services">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1 col-sm-12 text-center feature-title">
+
+                <h2 id="privacy-policy">Privacy Policy</h2>
+                <p>Our commitment to protecting your personal information.</p>
+            </div>
+        </div>
+        <div class="row row-feat">
+            <div class="col-md-12">
+                <h3>1. Information We Collect</h3>
+                <p>We collect personal information about you to provide the services you request, to improve the user’s interaction with Courier Notify, and to communicate with you about shipping information, services, promotions and contests. We try to limit the collected information to support the intended purpose of collection.</p>
+
+                <h4>1.1. Personal Information</h4>
+                <p>We collect personal information from you in a variety of ways when you interact with Courier Notify through the Website and Mobile Application. Some examples are when you:</p>
+
+                <ul>
+                    <li>Submit notification;</li>
+                    <li>Request courier tracking history;</li>
+                    <li>Request customer service or contact us;</li>
+                    <li>Otherwise submit personal information to Courier Notify.</li>
+                </ul>
+
+                <p>When you ask us to provide services for the benefit of a third party, you shall be legally authorized by said third party to disclose his/her personal information to Courier Notify. This authorization shall cover personal information including but not limited to the name, address, phone number, email address. You agree that you must bear all risks associated with the use of any third party’s personal information communicated by you and that under no circumstances will Courier Notify be liable in any way for any personal information or for any loss or damage of any kind incurred as a result of the use of any personal information. Courier Notify is merely acting as a passive channel for the use of the personal information and is not undertaking any obligation or liability relating to any third party’s personal information communicated by you and/or your company.</p>
+
+                <h4>1.2. Usage Information</h4>
+                <p>We receive and store certain types of information about your use of the Website or Mobile Application when you use the Website or Mobile Application. Our purpose is to allow the Website or Mobile Application to work correctly, to allow us to provide our services, to evaluate use of the Website, for marketing purposes and to personalize and enhance your interaction with Courier Notify. Some examples include:</p>
+                <p>We may collect technical information such as your IP address, the address of a referring website or application, the type of web browser or mobile device you use, your operating system, your Internet Service Provider and the path you take through the Website; </p>
+                <p>When you purchase services, we collect information about the transaction, such as services details and the date of the purchase/return.</p>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
